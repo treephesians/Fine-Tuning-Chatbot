815c5281eb8e508d9f7cfdeefbc768d9e4e5ccc0
treephesians <136706252+treephesians@users.noreply.github.com>
2023-07-18 10:17:57 +0900

    Setting

--- app/views.py
+++ app/views.py
@@ -1,14 +1,15 @@
+from django.shortcuts import render
 from django.http import HttpResponse
-from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
 def hello(request):
    return HttpResponse("Hello, World!")
-
 
 @api_view(['GET', 'POST'])
 @permission_classes([AllowAny])
 def hello_rest_api(request):
     data = {'message': 'Hello, REST API!'}
     return Response(data)
+# Create your views here.
