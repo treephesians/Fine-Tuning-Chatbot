08981adc632fe2c6897d5214675cec7f4a31d3dd
treephesians <136706252+treephesians@users.noreply.github.com>
2023-07-19 17:18:04 +0900

    chapter 03

--- backend/fine_tuning_chatbot/admin.py
+++ backend/fine_tuning_chatbot/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import FineTunedModel, TrainingData
 
-# Register your models here.
+@admin.register(FineTunedModel)
+class FineTunedModelAdmin(admin.ModelAdmin):
+    list_display = ('model_name', 'base_model')
+    search_fields = ('model_name', 'base_model')
+
+@admin.register(TrainingData)
+class TrainingDataAdmin(admin.ModelAdmin):
+    list_display = ('prompt', 'completion', 'fine_tuned_model')
+    search_fields = ('prompt', 'completion', 'fine_tuned_model__model_name')
+    list_filter = ('fine_tuned_model',)
--- backend/fine_tuning_chatbot/models.py
+++ backend/fine_tuning_chatbot/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class FineTunedModel(models.Model):
+    MODEL_CHOICES = [
+        ('ada', 'Ada'),
+        ('babbage', 'Babbage'),
+        ('curie', 'Curie'),
+        ('davinci', 'Davinci'),
+    ]
+
+    model_name = models.CharField(max_length=100)
+    base_model = models.CharField(max_length=100, choices=MODEL_CHOICES)
+
+    def __str__(self):
+        return self.model_name
+
+class TrainingData(models.Model):
+    fine_tuned_model = models.ForeignKey(FineTunedModel, on_delete=models.CASCADE, related_name='training_data')
+    prompt = models.TextField()
+    completion = models.TextField()
+
+    def __str__(self):
+        return f"{self.fine_tuned_model.model_name}의 훈련 데이터"
